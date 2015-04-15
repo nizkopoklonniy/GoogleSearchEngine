58fbbeed6100c0f0720bb67d8786cd99a7ad5f8f
dnizkop <dmitriy.nizkopoklonniy@itnent.com>
2015-04-15 13:05:00 +0300

    Refractor code.

--- src/test/java/GoogleTests/TestConfig.java
+++ src/test/java/GoogleTests/TestConfig.java
@@ -6,21 +6,24 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+/**
+ * Test configuration.
+ */
 public class TestConfig {
-	@BeforeMethod
-	public void beforeMethod() {
-		webDriver = new FirefoxDriver();
-	}
+    @BeforeMethod
+    public void beforeMethod() {
+        webDriver = new FirefoxDriver();
+    }
 
-	/**
-	 * WebDriver instance.
-	 */
-	protected WebDriver webDriver;
+    /**
+     * WebDriver instance.
+     */
+    protected WebDriver webDriver;
 
-	@AfterMethod
-	public void afterMethod() {
+    @AfterMethod
+    public void afterMethod() {
 
-		webDriver.quit();
-		webDriver = null;
-	}
+        webDriver.quit();
+        webDriver = null;
+    }
 }
